15bb29d306a708db5bead4d6cab7772fb3f699fd
SelmaKongstad <s255185@dtu.dk>
2026-09-08 15:21:40 +0200

    aller sidste

--- free_recall_baseline.py
+++ free_recall_baseline.py
@@ -71,7 +71,7 @@ def run_experiment():
         name = input("Enter participant name: ")
 
         # Prepare CSV file
-        with open(f"Result/free_recall_baseline{name}.csv", "w", newline="", encoding="utf-8") as f:
+        with open(f"Result/free_recall_baseline_{name}.csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow([
                 "participant",
@@ -79,7 +79,7 @@ def run_experiment():
                 "presented_words",
                 "recalled_words",
                 "serial_position_score"
-                ])
+            ])
             
             # 10 trials per participant
             for trial in range(1, 11):
